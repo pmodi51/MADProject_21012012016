dd683c45e05c60bbbca1cfcc84b0926a2c0a5e6c
sikandargit <sikandardamor21@gnu.ac.in>
2022-08-06 14:37:54 +0530

    practical-1

---
+++ src/main/kotlin/practical-1.kt
@@ -0,0 +1,20 @@
+fun main(){
+    val a:Int=42
+    val b:Float=45.45f
+    val c:Char='T'
+    val d:String= "HMS"
+    val e:Boolean= false
+    val f:Double=96.36
+    val g:Long=315464321
+    val h:Short=-2
+    val i:Byte=127
+    println("Integer value : $a")
+    println("Float value : $b")
+    println("Character value : $c")
+    println("String value : $d")
+    println("Boolean value : $e")
+    println("Double value : $f")
+    println("Long value : $g")
+    println("Short value : $h")
+    println("Byte value : $i")
+}
